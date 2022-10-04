00e5ab4f86eb44e3fcf13191eba57353280d1368
Trivium-bit <trivium.exe@gmail.com>
2022-10-04 14:26:22 +0300

    add @iconify/react

--- src/components/Input/Input.tsx
+++ src/components/Input/Input.tsx
@@ -1,12 +1,9 @@
 import React, { ChangeEvent, DetailedHTMLProps, Dispatch, InputHTMLAttributes, KeyboardEvent, SetStateAction } from "react";
 import s from "./Input.module.css";
 
-// тип пропсов обычного инпута
 type DefaultInputPropsType = DetailedHTMLProps<InputHTMLAttributes<HTMLInputElement>, HTMLInputElement>;
 
-// здесь мы говорим что у нашего инпута будут такие же пропсы как у обычного инпута
-// (чтоб не писать value: string, onChange: ...; они уже все описаны в DefaultInputPropsType)
-type SuperInputTextPropsType = Omit<DefaultInputPropsType, 'onChange'> & { // и + ещё пропсы которых нет в стандартном инпуте
+type SuperInputTextPropsType = Omit<DefaultInputPropsType, 'onChange'> & {
     onChangeText?: (value: string) => void
     onEnter?: () => void
     error?: string
@@ -14,33 +11,30 @@ type SuperInputTextPropsType = Omit<DefaultInputPropsType, 'onChange'> & { // и
     onChange?: Dispatch<SetStateAction<string>>
 };
 
-const SuperInputText: React.FC<SuperInputTextPropsType> = (
+const Input: React.FC<SuperInputTextPropsType> = (
     {
-        type, // достаём и игнорируем чтоб нельзя было задать другой тип инпута
+        type,
         onChange, onChangeText,
         onKeyPress, onEnter,
         error,
         className, spanClassName,
-
-        ...restProps// все остальные пропсы попадут в объект restProps
+        ...restProps
     }
 ) => {
     const onChangeCallback = (e: ChangeEvent<HTMLInputElement>) => {
-        onChange // если есть пропс onChange
-            && onChange(e.currentTarget.value); // то передать ему е (поскольку onChange не обязателен)
-
+        onChange && onChange(e.currentTarget.value);
         onChangeText && onChangeText(e.currentTarget.value);
     }
     const onKeyPressCallback = (e: KeyboardEvent<HTMLInputElement>) => {
         onKeyPress && onKeyPress(e);
 
-        e.key === "Enter" // если нажата кнопка Enter
-            && onEnter // и есть пропс onEnter
-            && onEnter(); // то вызвать его
+        e.key === "Enter"
+            && onEnter
+            && onEnter();
     }
 
     const finalSpanClassName = `${s.error} ${spanClassName ? spanClassName : ""}`;
-    const finalInputClassName = `${s.input} ${error ? s.errorInput : s.superInput} ${className}`; // need to fix with (?:) and s.superInput
+    const finalInputClassName = `${s.input} ${error ? s.errorInput : s.superInput} ${className}`;
 
     return (
         <>
@@ -49,12 +43,12 @@ const SuperInputText: React.FC<SuperInputTextPropsType> = (
                 onChange={onChangeCallback}
                 onKeyPress={onKeyPressCallback}
                 className={finalInputClassName}
-
-                {...restProps} // отдаём инпуту остальные пропсы если они есть (value например там внутри)
+                placeholder={"Введите имя, тег, почту..."}
+                {...restProps}
             />
             {error && <span className={finalSpanClassName}>{error}</span>}
         </>
     );
 }
 
-export default SuperInputText;
+export default Input;
--- src/components/homePage/SearchBar/SearchBar.tsx
+++ src/components/homePage/SearchBar/SearchBar.tsx
@@ -1,14 +1,18 @@
 import React, { useState } from 'react';
 import style from './SearchBar.module.css'
+import { Icon } from '@iconify/react';
+import Input from '../../Input/Input';
 
 function SearchBar() {
-  const [text, setText] = useState("");
-  const handleChange = (event: any) => {
-    setText(event.target.value);
-  };
   return (
     <div className={style.searchLine}>
-      <></>
+      <div className={style.search}>
+      <Icon icon="eva:search-fill" color="#c3c3c6" width="20" height="20" />
+      <Input />
+      </div>
+      <div className={style.sort}>
+      <Icon icon="material-symbols:sort-rounded" color="#c3c3c6" width="26" height="20" hFlip={true} />
+      </div>
     </div>
   );
 }
